f1643b7580b89bc2da403ea40540b0b8f071767a
sfprakash <prakash.sf4626@gmail.com>
2024-05-03 13:17:40 +0530

    updating typeScript files

--- OnlineMedicalStore/script.js
+++ OnlineMedicalStore/script.js
@@ -1,6 +1,9 @@
 var UserIdAutoIncrement = 1000;
 var MedicineIdAutoIncrement = 10;
+var OrderIdAutoIncrement = 100;
 var CurrentUserId;
+var CurrentMedicineId;
+var CurrentloggedInUser;
 var User = /** @class */ (function () {
     function User(paramUserEmail, paramUserPassword, paramUserPhone) {
         UserIdAutoIncrement++;
@@ -8,7 +11,14 @@ var User = /** @class */ (function () {
         this.UserEmail = paramUserEmail;
         this.UserPassword = paramUserPassword;
         this.UserPhone = paramUserPhone;
+        this.WalletBalance = 0;
     }
+    User.prototype.rechargeWallet = function (paramAmount) {
+        this.WalletBalance += paramAmount;
+    };
+    User.prototype.showBalance = function () {
+        return this.WalletBalance;
+    };
     return User;
 }());
 var MedicineInfo = /** @class */ (function () {
@@ -22,14 +32,34 @@ var MedicineInfo = /** @class */ (function () {
     }
     return MedicineInfo;
 }());
+var OrderStatus;
+(function (OrderStatus) {
+    OrderStatus[OrderStatus["Ordered"] = 0] = "Ordered";
+    OrderStatus[OrderStatus["Cancelled"] = 1] = "Cancelled";
+})(OrderStatus || (OrderStatus = {}));
+var Order = /** @class */ (function () {
+    function Order(paramMedicineId, paramUserId, paramMedicineName, paramMedicineCount, paramTotalPrice, paramOrderStatus) {
+        OrderIdAutoIncrement++;
+        this.OrderId = "OI" + OrderIdAutoIncrement.toString();
+        this.MedicineId = paramMedicineId;
+        this.UserId = paramUserId;
+        this.MedicineName = paramMedicineName;
+        this.MedicineCount = paramMedicineCount;
+        this.TotalPrice = paramTotalPrice;
+        this.OrderStatus = paramOrderStatus;
+    }
+    return Order;
+}());
 var UserArrayList = new Array();
-UserArrayList.push(new User("Prakash", "191101", 9361415123));
+UserArrayList.push(new User("Prakash@gmail.com", "password", 9361415123));
 var MedicineList = new Array();
 MedicineList.push(new MedicineInfo("Paracitamol", 40, 5, new Date("2024-06-30")));
 MedicineList.push(new MedicineInfo("Calpol", 10, 5, new Date("2024-05-30")));
 MedicineList.push(new MedicineInfo("Gelusil", 3, 40, new Date("2024-04-30")));
 MedicineList.push(new MedicineInfo("Metrogel", 5, 50, new Date("2024-12-30")));
 MedicineList.push(new MedicineInfo("Povidin Iodin", 10, 50, new Date("2024-10-30")));
+var OrderList = new Array();
+OrderList.push(new Order("UI1001", "MD14", "Metrogel", 3, 150, OrderStatus.Ordered));
 function homePage() {
     var homePage = document.getElementById("homePage");
     homePage.style.display = "block";
@@ -70,6 +100,9 @@ function SignIn() {
         for (var i = 0; i < UserArrayList.length; i++) {
             if (UserArrayList[i].UserId == existingUserId) {
                 noExistingUserIdChecker = false;
+                CurrentUserId = existingUserId;
+                CurrentloggedInUser = UserArrayList[i];
+                alert("logged-in");
                 medicinePage();
                 return;
             }
@@ -83,20 +116,94 @@ function SignIn() {
     }
 }
 function medicinePage() {
+    var homePage = document.getElementById("homePage");
     var existingUserPage = document.getElementById('existingUserPage');
     var medicinePage = document.getElementById('medicinePage');
     var greet = document.getElementById('greet');
+    homePage.style.display = "none";
     existingUserPage.style.display = "none";
     medicinePage.style.display = "block";
     // greet.innerHTML = `<h3>Hello ${CurrentUserName}</h3>`;
 }
 function showMedicine() {
+    var medicineInfoPage = document.getElementById("medicine-info");
+    medicineInfoPage.style.display = "block";
     var table = document.getElementById("dataTable");
     var medicineBtn = document.getElementById("medicine-btn");
+    var heading = document.createElement("tr");
+    heading.innerHTML = "<th>".concat("MedicineID", "</th><th>").concat("Medicine Name", "</th><th>").concat("Medicine Count", "</th><th>").concat("Medicine Price", "</th><th>").concat("Expiry Date", "</th>");
+    table.appendChild(heading);
     for (var i = 0; i < MedicineList.length; i++) {
         var row = document.createElement("tr");
         row.innerHTML = "<td>".concat(MedicineList[i].MedicineId, "</td>\n        <td>").concat(MedicineList[i].MedicineName, "</td>\n        <td>").concat(MedicineList[i].MedicineCount, "</td>\n        <td>").concat(MedicineList[i].MedicinePrice, "</td>\n        <td>").concat(MedicineList[i].MedicineExpiryDate.toString(), "</td>");
         table.appendChild(row);
     }
     medicineBtn.disabled = true;
+}
+function Purchase() {
+    var medicineInfoPage = document.getElementById("medicine-info");
+    medicineInfoPage.style.display = "none";
+    var purchasePage = document.getElementById("purchase-block");
+    purchasePage.style.display = "block";
+    var table = document.getElementById("purchase-table");
+    var purchaseBtn = document.getElementById("purchase-btn");
+    for (var i = 0; i < MedicineList.length; i++) {
+        var row = document.createElement("tr");
+        row.innerHTML = "<td>".concat(MedicineList[i].MedicineId, "</td>\n        <td>").concat(MedicineList[i].MedicineName, "</td>\n        <td>").concat(MedicineList[i].MedicineCount, "</td>\n        <td>").concat(MedicineList[i].MedicinePrice, "</td>\n        <td>").concat(MedicineList[i].MedicineExpiryDate.toString(), "</td>\n        <td><button onclick=\"setGlobalMedicineId('").concat(MedicineList[i].MedicineId, "');\">buy</button></td>");
+        table.appendChild(row);
+    }
+    purchaseBtn.disabled = true;
+}
+function setGlobalMedicineId(MedicineId) {
+    CurrentMedicineId = MedicineId;
+    var quantityBlock = document.getElementById("quantity-block");
+    quantityBlock.style.display = "block";
+}
+function getQuantity() {
+    var quantity = parseInt(document.getElementById("quantity").value);
+    buyMedicine(quantity);
+}
+function buyMedicine(quantity) {
+    for (var i = 0; i < MedicineList.length; i++) {
+        if (MedicineList[i].MedicineId == CurrentMedicineId) {
+            MedicineList[i].MedicineCount -= quantity;
+            var totalPrice = quantity * MedicineList[i].MedicinePrice;
+            var newOrder = new Order(MedicineList[i].MedicineId, CurrentUserId, MedicineList[i].MedicineName, quantity, totalPrice, OrderStatus.Ordered);
+            OrderList.push(newOrder);
+            alert("Ok");
+        }
+    }
+}
+function CancelOrder() {
+    var cancelOrderTable = document.getElementById("cancel-order");
+    var cancelOrderBtn = document.getElementById("cancel-order-btn");
+    for (var i = 0; i < OrderList.length; i++) {
+        var row = document.createElement("tr");
+        row.innerHTML = "<td>".concat(OrderList[i].OrderId, "</td>\n        <td>").concat(OrderList[i].UserId, "</td>\n        <td>").concat(OrderList[i].MedicineId, "</td>\n        <td>").concat(OrderList[i].MedicineName, "</td>\n        <td>").concat(OrderList[i].MedicineCount, "</td>\n        <td>").concat(OrderList[i].TotalPrice, "</td>\n        <td>").concat(OrderList[i].OrderStatus, "</td>\n        <td><button onclick=\"OrderCancel('").concat(OrderList[i].OrderId, "')\";>Cancel</button></td>");
+        cancelOrderTable.appendChild(row);
+    }
+    cancelOrderBtn.disabled = true;
+}
+function OrderCancel(OrderId) {
+    for (var i = 0; i <= OrderList.length; i++) {
+        if (OrderList[i].OrderId == OrderId) {
+            OrderList[i].OrderStatus = OrderStatus.Cancelled;
+            alert("Order Cancelled Successfully");
+            return;
+        }
+    }
+}
+function OrderHistory() {
+    var orderHistoryTable = document.getElementById("order-history");
+    var orderHistoryBtn = document.getElementById("order-history-btn");
+    for (var i = 0; i < OrderList.length; i++) {
+        var row = document.createElement("tr");
+        row.innerHTML = "<td>".concat(OrderList[i].OrderId, "</td>\n        <td>").concat(OrderList[i].UserId, "</td>\n        <td>").concat(OrderList[i].MedicineId, "</td>\n        <td>").concat(OrderList[i].MedicineName, "</td>\n        <td>").concat(OrderList[i].MedicineCount, "</td>\n        <td>").concat(OrderList[i].TotalPrice, "</td>\n        <td>").concat(OrderList[i].OrderStatus, "</td>");
+        orderHistoryTable.appendChild(row);
+    }
+    orderHistoryBtn.disabled = true;
+}
+function getAmount() {
+    var amount = parseInt(document.getElementById("top-up-amount").value);
+    CurrentloggedInUser.rechargeWallet(amount);
 }
--- OnlineMedicalStore/script.ts
+++ OnlineMedicalStore/script.ts
@@ -1,7 +1,10 @@
 let UserIdAutoIncrement = 1000;
 let MedicineIdAutoIncrement = 10;
+let OrderIdAutoIncrement = 100;
 
 let CurrentUserId:string;
+let CurrentMedicineId:string;
+let CurrentloggedInUser:User;
 
 
 class User{
@@ -9,6 +12,7 @@ class User{
     UserEmail:string;
     UserPassword:string;
     UserPhone:number;
+    WalletBalance:number;
 
     constructor(paramUserEmail:string, paramUserPassword:string, paramUserPhone:number)
     {
@@ -19,7 +23,19 @@ class User{
         this.UserEmail = paramUserEmail;
         this.UserPassword = paramUserPassword;
         this.UserPhone = paramUserPhone;
+        this.WalletBalance = 0;
     }
+
+    rechargeWallet(paramAmount:number)
+    {
+        this.WalletBalance += paramAmount;
+    }
+
+    showBalance()
+    {
+        return this.WalletBalance;
+    }
+
 }
 
 class MedicineInfo {
@@ -42,8 +58,32 @@ class MedicineInfo {
 
 }
 
+enum OrderStatus { Ordered, Cancelled}
+
+class Order {
+    OrderId: string;
+    MedicineId: string;
+    UserId: string;
+    MedicineName: string;
+    MedicineCount: number;
+    TotalPrice: number;
+    OrderStatus: OrderStatus;
+
+    constructor(paramMedicineId: string, paramUserId: string, paramMedicineName: string, paramMedicineCount: number, paramTotalPrice: number, paramOrderStatus:OrderStatus) {
+        OrderIdAutoIncrement++;
+
+        this.OrderId = "OI" + OrderIdAutoIncrement.toString();
+        this.MedicineId = paramMedicineId;
+        this.UserId = paramUserId;
+        this.MedicineName = paramMedicineName;
+        this.MedicineCount = paramMedicineCount;
+        this.TotalPrice = paramTotalPrice;
+        this.OrderStatus = paramOrderStatus;
+    }
+}
+
 let UserArrayList:Array<User> = new Array<User>();
-UserArrayList.push(new User("Prakash","191101",9361415123));
+UserArrayList.push(new User("Prakash@gmail.com","password",9361415123));
 
 let MedicineList: Array<MedicineInfo> = new Array<MedicineInfo>();
 MedicineList.push(new MedicineInfo("Paracitamol",40,5,new Date("2024-06-30")));
@@ -52,6 +92,8 @@ MedicineList.push(new MedicineInfo("Gelusil",3,40,new Date("2024-04-30")));
 MedicineList.push(new MedicineInfo("Metrogel",5,50,new Date("2024-12-30")));
 MedicineList.push(new MedicineInfo("Povidin Iodin",10,50,new Date("2024-10-30")));
 
+let OrderList: Array<Order> = new Array<Order>();
+OrderList.push(new Order("UI1001","MD14","Metrogel",3,150,OrderStatus.Ordered));
 
 function homePage()
 {
@@ -114,6 +156,9 @@ function SignIn()
             if(UserArrayList[i].UserId == existingUserId)
             {
                 noExistingUserIdChecker = false;
+                CurrentUserId = existingUserId;
+                CurrentloggedInUser = UserArrayList[i];
+                alert("logged-in");
                 medicinePage();
                 return;
             }
@@ -130,10 +175,12 @@ function SignIn()
 
 function medicinePage() {
 
+    let homePage = document.getElementById("homePage") as HTMLDivElement;
     let existingUserPage = document.getElementById('existingUserPage') as HTMLDivElement;
     let medicinePage = document.getElementById('medicinePage') as HTMLDivElement;
     let greet = document.getElementById('greet') as HTMLLabelElement;
 
+    homePage.style.display="none";
     existingUserPage.style.display = "none";
     medicinePage.style.display = "block";
 
@@ -143,8 +190,14 @@ function medicinePage() {
 
 function showMedicine(){
     
+    let medicineInfoPage = document.getElementById("medicine-info") as HTMLDivElement;
+    medicineInfoPage.style.display="block";
+
     let table = document.getElementById("dataTable") as HTMLTableElement;
     let medicineBtn = document.getElementById("medicine-btn") as HTMLButtonElement;
+    let heading = document.createElement("tr");
+    heading.innerHTML=`<th>${"MedicineID"}</th><th>${"Medicine Name"}</th><th>${"Medicine Count"}</th><th>${"Medicine Price"}</th><th>${"Expiry Date"}</th>`;
+    table.appendChild(heading);
     for(let i=0; i<MedicineList.length; i++)
     {
         let row = document.createElement("tr");
@@ -157,4 +210,119 @@ function showMedicine(){
         table.appendChild(row);
     }
     medicineBtn.disabled=true;
+}
+
+function Purchase()
+{
+    let medicineInfoPage = document.getElementById("medicine-info") as HTMLDivElement;
+    medicineInfoPage.style.display="none";
+    let purchasePage = document.getElementById("purchase-block") as HTMLDivElement;
+    purchasePage.style.display="block";
+
+    let table = document.getElementById("purchase-table") as HTMLTableElement;
+    let purchaseBtn = document.getElementById("purchase-btn") as HTMLButtonElement;
+
+    for(let i=0; i<MedicineList.length; i++)
+    {
+        let row = document.createElement("tr");
+        row.innerHTML=`<td>${MedicineList[i].MedicineId}</td>
+        <td>${MedicineList[i].MedicineName}</td>
+        <td>${MedicineList[i].MedicineCount}</td>
+        <td>${MedicineList[i].MedicinePrice}</td>
+        <td>${MedicineList[i].MedicineExpiryDate.toString()}</td>
+        <td><button onclick="setGlobalMedicineId('${MedicineList[i].MedicineId}');">buy</button></td>`;
+
+        table.appendChild(row);
+    }
+    purchaseBtn.disabled=true;
+}
+function setGlobalMedicineId(MedicineId)
+{
+    CurrentMedicineId = MedicineId;
+    let quantityBlock = document.getElementById("quantity-block") as HTMLDivElement;
+    quantityBlock.style.display="block";
+    
+}
+function getQuantity()
+{
+    let quantity:number = parseInt((document.getElementById("quantity") as HTMLInputElement).value);
+    buyMedicine(quantity);
+}
+function buyMedicine(quantity:number)
+{
+    for(let i=0; i<MedicineList.length; i++)
+    {
+        if(MedicineList[i].MedicineId == CurrentMedicineId)
+        {
+            MedicineList[i].MedicineCount -= quantity;
+            let totalPrice = quantity * MedicineList[i].MedicinePrice;
+            let newOrder: Order = new Order(MedicineList[i].MedicineId,CurrentUserId,MedicineList[i].MedicineName,quantity,totalPrice,OrderStatus.Ordered);
+            OrderList.push(newOrder);
+            alert("Ok");
+        }
+    }
+}
+
+function CancelOrder()
+{
+    let cancelOrderTable = document.getElementById("cancel-order") as HTMLTableElement;
+    let cancelOrderBtn = document.getElementById("cancel-order-btn") as HTMLButtonElement;
+
+    for(let i=0; i<OrderList.length; i++)
+    {
+        let row = document.createElement("tr");
+        row.innerHTML=`<td>${OrderList[i].OrderId}</td>
+        <td>${OrderList[i].UserId}</td>
+        <td>${OrderList[i].MedicineId}</td>
+        <td>${OrderList[i].MedicineName}</td>
+        <td>${OrderList[i].MedicineCount}</td>
+        <td>${OrderList[i].TotalPrice}</td>
+        <td>${OrderList[i].OrderStatus}</td>
+        <td><button onclick="OrderCancel('${OrderList[i].OrderId}')";>Cancel</button></td>`;
+
+        cancelOrderTable.appendChild(row);
+    }
+    cancelOrderBtn.disabled=true;
+
+}
+
+function OrderCancel(OrderId)
+{
+    for(let i=0; i<=OrderList.length; i++)
+    {
+        if(OrderList[i].OrderId == OrderId)
+        {
+            OrderList[i].OrderStatus = OrderStatus.Cancelled;
+            alert("Order Cancelled Successfully");
+            return;
+        }
+    }
+}
+
+function OrderHistory()
+{
+    let orderHistoryTable = document.getElementById("order-history") as HTMLTableElement;
+    let orderHistoryBtn = document.getElementById("order-history-btn") as HTMLButtonElement;
+
+    for(let i=0; i<OrderList.length; i++)
+    {
+        let row = document.createElement("tr");
+        row.innerHTML=`<td>${OrderList[i].OrderId}</td>
+        <td>${OrderList[i].UserId}</td>
+        <td>${OrderList[i].MedicineId}</td>
+        <td>${OrderList[i].MedicineName}</td>
+        <td>${OrderList[i].MedicineCount}</td>
+        <td>${OrderList[i].TotalPrice}</td>
+        <td>${OrderList[i].OrderStatus}</td>`;
+
+        orderHistoryTable.appendChild(row);
+    }
+    orderHistoryBtn.disabled=true;
+
+}
+
+function getAmount()
+{
+    let amount =parseInt((document.getElementById("top-up-amount") as HTMLInputElement).value);
+    CurrentloggedInUser.rechargeWallet(amount);
 }
